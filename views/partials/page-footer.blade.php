68b440b9260cde4a31079268677270e4f4574d2a
Peter Antonius <peter.antonius@whitespace.se>
2016-09-20 11:10:21 +0200

    updated structure for page-footer.blade.php

--- views/partials/page-footer.blade.php
+++ views/partials/page-footer.blade.php
@@ -1,28 +1,17 @@
 <?php do_action('customer-feedback'); ?>
 
-<footer class="page-footer">
+<footer class="c-page-footer">
+    <div class="c-article-timestamps">
+        @include('partials.timestamps')
+    </div>
+
+
     @if (get_field('show_share', get_the_id()) !== false && get_field('page_show_share', 'option') !== false)
-    <div class="grid hidden-print">
-        <div class="grid-xs-12">
-            <div class="box box-border gutter gutter-horizontal no-margin">
-                <div class="gutter gutter-vertical gutter-sm">
-                <div class="grid grid-table grid-va-middle no-margin no-padding">
-                    <div class="grid-md-8">
-                        <i class="pricon pricon-share pricon-lg" style="margin-right:5px;"></i> <strong><?php _e('Share the page', 'municipio'); ?>:</strong> {{ the_title() }}
-                    </div>
-                    <div class="grid-md-4 text-right-md text-right-lg">
-                        @include('partials.social-share')
-                    </div>
-                </div>
-                </div>
-            </div>
-        </div>
+    <div class="c-share hidden-print clearfix">
+
+        <p class="c-share__title" id="social-share"><strong><?php _e('Share the page', 'municipio'); ?>:</strong> {{ the_title() }}</p>
+        @include('partials.social-share')
+
     </div>
     @endif
-
-    <div class="grid">
-        <div class="grid-xs-12">
-            @include('partials.timestamps')
-        </div>
-    </div>
 </footer>
