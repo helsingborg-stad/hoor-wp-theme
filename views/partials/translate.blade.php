6da8ab8024437583a29ee87cf2baa63641c09664
Peter Antonius <peter.antonius@whitespace.se>
2016-09-20 12:05:54 +0200

    updated structure for translate.blade.php initial style for Google Translate

--- views/partials/translate.blade.php
+++ views/partials/translate.blade.php
@@ -1,15 +1,7 @@
-<div id="translate" class="google-translate {{ get_field('show_google_translate', 'option') != 'fold' ? 'creamy creamy-border-bottom gutter gutter-padding gutter-vertical' : 'google-translate-fold' }} target-toggle">
-    <div class="container">
-        <div class="grid">
-            <div class="grid-sm-12">
-                <h3>Translate</h3>
-                <p>
-                    You can use Google Translate to translate the contents of Helsingborg.se. To do that, select the language you would like to translate into in the lost below.
-                    <br><small>Please bear in mind, since the Google Translate is an automatically generated translation, we do not take any responsibility for errors in the text.</small>
-                </p>
-                <div id="google-translate-element" class="gutter gutter-vertical"></div>
-                <a href="#" class="btn btn-primary">Close</a>
-            </div>
-        </div>
-    </div>
+<div id="translate" class="c-google-translate {{ get_field('show_google_translate', 'option') != 'fold' ? 'c-google-translate--static' : 'c-google-translate--fixed' }} target-toggle">
+    <h3>Translate</h3>
+    <p>You can use Google Translate to translate the contents of hoor.se. To do that, select the language you would like to translate into in the lost below.</p>
+    <p><small>Please bear in mind, since the Google Translate is an automatically generated translation, we do not take any responsibility for errors in the text.</small></p>
+    <div id="google-translate-element" class="gutter gutter-vertical"></div>
+    <button class="button button--close">Close</button>
 </div>
