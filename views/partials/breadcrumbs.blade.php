021fd3efe9fb29d5e2cfa4258ebbb11fe8e131e9
Peter Antonius <peter.antonius@whitespace.se>
2016-09-21 10:28:22 +0200

    show breadcrumb on level 1 pages remove unused grid structure

--- views/partials/breadcrumbs.blade.php
+++ views/partials/breadcrumbs.blade.php
@@ -1,6 +1,12 @@
-@if (wp_get_post_parent_id(get_the_id()) != 0)
+@if (wp_get_post_parent_id(get_the_id()) != 1)
 <nav class="c-breadcrumbs" aria-label="<?php _e('breadcrumbs', 'hoor'); ?>" role="navigation">
-    <p class="c-breadcrumbs__title hidden-print" id="breadcrumbslabel"><?php _e('You are here:', 'hoor'); ?></p>
-    {{ \Hoor\Theme\Navigation::outputBreadcrumbs() }}
+    <div class="container">
+        <div class="grid">
+            <div class="grid-md-12">
+                <p class="c-breadcrumbs__title hidden-print" id="breadcrumbslabel"><?php _e('You are here:', 'hoor'); ?></p>
+                {{ \Hoor\Theme\Navigation::outputBreadcrumbs() }}
+            </div>
+        </div>
+    </div>
 </nav>
 @endif
