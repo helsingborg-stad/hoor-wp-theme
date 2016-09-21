0f54caa53a3057a111d8b43927fe57886942a03b
Peter Antonius <peter.antonius@whitespace.se>
2016-09-21 22:52:58 +0200

    Unregister stale and unused WordPress widgets

--- functions.php
+++ functions.php
@@ -71,3 +71,22 @@ add_filter('embed_oembed_html', 'site_embed_oembed_html', 99, 4);
 function site_embed_oembed_html($html, $url, $attr, $post_id) {
     return '<div class="o-embed-container">' . $html . '</div>';
 }
+
+// Unregister stale and unused WordPress widgets
+function hoor_unregister_widgets() {
+    unregister_widget('WP_Widget_Tag_Cloud');
+    unregister_widget('WP_Widget_Meta');
+    unregister_widget('WP_Widget_Categories');
+    unregister_widget('WP_Widget_Recent_Posts');
+    unregister_widget('WP_Widget_Recent_Comments');
+    unregister_widget('WP_Widget_Search');
+    unregister_widget('WP_Widget_Calendar');
+    unregister_widget('WP_Widget_Archives');
+    unregister_widget('WP_Widget_Links');
+    unregister_widget('WP_Widget_Text');
+
+    //unregister_widget('WP_Widget_Pages');
+    //unregister_widget('WP_Nav_Menu_Widget');
+    //unregister_widget('WP_Widget_RSS');
+}
+add_action( 'widgets_init', 'hoor_unregister_widgets' );
