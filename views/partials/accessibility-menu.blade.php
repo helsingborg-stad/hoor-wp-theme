fbdcf40366c172d98c66b9345fcae4a264025f57
Peter Antonius <peter.antonius@whitespace.se>
2016-09-21 12:12:41 +0200

    style tweaks to Readspeaker button

--- views/partials/accessibility-menu.blade.php
+++ views/partials/accessibility-menu.blade.php
@@ -1,11 +1,9 @@
 @if (function_exists('ReadSpeakerHelper_playButton'))
-<div class="c-accessibility-menu" aria-label="<?php _e('Listen to this page text with ReadSpeaker', 'hoor'); ?>">
+<div class="c-accessibility-menu" aria-label="<?php _e('Listen to this page with ReadSpeaker', 'hoor'); ?>">
     {!! ReadSpeakerHelper_playButton() !!}
 </div>
 @endif
 
 @if (function_exists('ReadSpeakerHelper_player'))
-    <div>
     {!! ReadSpeakerHelper_player() !!}
-    </div>
 @endif
