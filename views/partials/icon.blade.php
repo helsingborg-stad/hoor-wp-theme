a5b382445a866599aeeb08d678e03aceaa942a12
Peter Antonius <peter.antonius@whitespace.se>
2016-09-27 09:18:20 +0200

    Adds dummy icons to project.

---
+++ views/partials/icon.blade.php
@@ -0,0 +1,1 @@
+<svg class="o-icon o-icon--{{ $name }}"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="<?php echo get_stylesheet_directory_uri() ?>/assets/dist/images/icons.svg#icons--{{ $name }}"></use></svg>
