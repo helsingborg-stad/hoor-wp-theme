65106f7affbd634923be8f9a8a37bf4ad7f7c99d
Peter Antonius <peter.antonius@whitespace.se>
2016-09-28 21:56:49 +0200

    Added Readspeaker to Single Posts

---
+++ views/partials/blog/post-header.blade.php
@@ -0,0 +1,7 @@
+<header class="post-header">
+    <h1><a href="{{ the_permalink() }}">{{ the_title() }}</a></h1>
+
+    @include('partials.accessibility-menu')
+
+    @include('partials.blog.post-info')
+</header>
