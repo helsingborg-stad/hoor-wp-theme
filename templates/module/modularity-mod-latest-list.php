adcdd5c4b52781e226ba16f8f7e7150094e36abe
Peter Antonius <peter.antonius@whitespace.se>
2016-09-28 21:54:24 +0200

    BEM classes on Latest List

--- templates/module/modularity-mod-latest-list.php
+++ templates/module/modularity-mod-latest-list.php
@@ -1,7 +1,7 @@
 <?php if (count($posts) > 0): ?>
-    <div class="<?php echo implode(' ', apply_filters('Modularity/Module/Classes', array('box', 'box-panel'), $module->post_type, $args)); ?>">
-        <h4 class="box-title"><?php echo $module->post_title; ?></h4>
-        <ul>
+    <div class="<?php echo implode(' ', apply_filters('Modularity/Module/Classes', array('box', 'box--inline'), $module->post_type, $args)); ?>">
+        <h4 class="box__headline"><?php echo $module->post_title; ?></h4>
+        <ul class="box__list">
             <?php
             foreach ($posts as $post) :
 
@@ -17,19 +17,20 @@
                     $meta_data = "";
                 }
             ?>
-                <li data-meta-sort-by="<?php echo $meta_data; ?>">
-                    <a href="<?php echo get_permalink($post->ID); ?>" data-meta-sort-by="<?php echo $meta_data; ?>">
+                <li class="box__list-item" data-meta-sort-by="<?php echo $meta_data; ?>">
+                    <a class="box__list-link" href="<?php echo get_permalink($post->ID); ?>" data-meta-sort-by="<?php echo $meta_data; ?>">
                         <?php if ($fields->show_title) : ?>
-                            <span class="link-item title"><?php echo apply_filters('the_title', $post->post_title); ?></span>
+                            <span class="box__title"><?php echo apply_filters('the_title', $post->post_title); ?></span>
                         <?php endif; ?>
 
                         <?php if ($fields->show_date) : ?>
-                        <time class="date text-sm text-dark-gray"><?php echo get_the_time('Y-m-d', $post->ID); ?></time>
+                        <time class="box__date"><?php echo get_the_time('Y-m-d', $post->ID); ?></time>
                         <?php endif; ?>
                     </a>
                 </li>
             <?php endforeach; ?>
-
+        </ul>
+        <footer class="box__footer">
             <?php if ($showMoreButton) : ?>
                 <?php
                     $filters = array(
@@ -50,8 +51,8 @@
                         $filters['term'] = $taxValues;
                     }
                 ?>
-                <li><a class="read-more" href="<?php echo get_post_type_archive_link($fields->post_type); echo '?' . http_build_query($filters); ?>"><?php _e('Show more', 'modularity'); ?></a></li>
+                <a class="o-button o-button--primary" href="<?php echo get_post_type_archive_link($fields->post_type); echo '?' . http_build_query($filters); ?>"><?php _e('Show more', 'modularity'); ?></a>
             <?php endif; ?>
-        </ul>
+        </footer>
     </div>
 <?php endif ?>
