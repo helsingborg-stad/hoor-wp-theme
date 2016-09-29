f6d1b40d499a7b133c4a2d6f876555aa0b4ac318
Peter Antonius <peter.antonius@whitespace.se>
2016-09-29 18:21:10 +0200

    Small visual tweaks Google Translate style fixes

--- views/partials/translate.blade.php
+++ views/partials/translate.blade.php
@@ -1,7 +1,11 @@
 <div id="translate" class="c-google-translate {{ get_field('show_google_translate', 'option') != 'fold' ? 'c-google-translate--static' : 'c-google-translate--fixed' }} target-toggle hidden-print">
-    <h3>Translate</h3>
-    <p>You can use Google Translate to translate the contents of hoor.se. To do that, select the language you would like to translate into in the lost below.</p>
-    <p><small>Please bear in mind, since the Google Translate is an automatically generated translation, we do not take any responsibility for errors in the text.</small></p>
-    <div id="google-translate-element"></div>
-    <a href="#" class="o-button o-button--close">Close</a>
+    <div class="c-google-translate__container">
+        <h3 class="c-google-translate__headline">Translate</h3>
+        <div class="c-google-translate__content">
+            <p class="c-google-translate__description">You can use Google Translate to translate the contents of hoor.se. To do that, select the language you would like to translate into in the lost below.</p>
+            <p class="c-google-translate__disclaimer">Please bear in mind, since the Google Translate is an automatically generated translation, we do not take any responsibility for errors in the text.</p>
+            <div id="google-translate-element"></div>
+        </div>
+        <a href="#" class="c-google-translate__button" aria-label="Close popup window"></a>
+    </div>
 </div>
