c1e93a313d00cd41f88a7e37b460211b66c5ea68
Anders Fajerson <anders@perifer.se>
2016-09-29 12:23:12 +0200

    Add header to sidebar menu
    Unfortunately it became to cumbersome to also alter the nav html, e.g
    to add aria aria-labelledby.

---
+++ views/partials/sidebar-left.blade.php
@@ -0,0 +1,32 @@
+@if ($hasLeftSidebar)
+<aside class="grid-md-4 grid-lg-3 sidebar-left-sidebar hidden-print">
+    @if (is_active_sidebar('left-sidebar'))
+        <div class="grid sidebar-left-sidebar-top">
+            <?php dynamic_sidebar('left-sidebar'); ?>
+        </div>
+    @endif
+
+    @if (get_field('nav_sub_enable', 'option'))
+        <?php
+            global $post;
+            $ancestors = array_reverse(get_post_ancestors($post->ID));
+            if ($ancestors) {
+                $ancestor = $ancestors[0];
+                $sidebar_label = get_the_title($ancestor);
+            } else {
+                $sidebar_label = get_the_title($post->ID);
+            }
+
+        ?>
+
+        <h2 class="sidebar-menu__headline"><span class="visually-hidden">Undernavigation för </span>{{ $sidebar_label }}</h2>
+        {!! $navigation['sidebarMenu'] !!}
+    @endif
+
+    @if (is_active_sidebar('left-sidebar-bottom'))
+        <div class="grid sidebar-left-sidebar-bottom">
+            <?php dynamic_sidebar('left-sidebar-bottom'); ?>
+        </div>
+    @endif
+</aside>
+@endif
