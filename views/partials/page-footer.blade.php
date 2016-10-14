10990dbc795e6083444c06a035e3ecceec832998
Peter Antonius <peter.antonius@whitespace.se>
2016-10-14 14:28:19 +0200

    Layout tweaks to share and timestamp

--- views/partials/page-footer.blade.php
+++ views/partials/page-footer.blade.php
@@ -1,17 +1,16 @@
 <?php do_action('customer-feedback'); ?>
 
 <footer class="c-page-footer">
-    <div class="c-article-timestamps">
-        @include('partials.timestamps')
-    </div>
-
-
     @if (get_field('show_share', get_the_id()) !== false && get_field('page_show_share', 'option') !== false)
     <div class="c-share hidden-print clearfix">
 
-        <p class="c-share__title" id="social-share"><strong><?php _e('Share the page', 'municipio'); ?>:</strong> {{ the_title() }}</p>
+        <p class="c-share__title visually-hidden" id="social-share"><strong><?php _e('Share the page', 'municipio'); ?>:</strong> {{ the_title() }}</p>
         @include('partials.social-share')
 
     </div>
     @endif
+
+    <div class="c-article-timestamps">
+        @include('partials.timestamps')
+    </div>
 </footer>
--- views/partials/timestamps.blade.php
+++ views/partials/timestamps.blade.php
@@ -1,27 +1,4 @@
 <ul class="c-article-timestamps__list">
-    @if (get_field('page_show_author', 'option') !== false && get_field('post_show_author', get_the_id()) !== false && get_the_author())
-        <li class="c-article-timestamps__list-item">
-            <strong><?php echo apply_filters('Municipio/author_display/title', __('Published by', 'municipio')); ?>:</strong>
-            <span class="post-author post-author-margin-left">
-                @if (get_field('page_show_author_image', 'option') !== false && get_field('post_show_author_image', get_the_id()) !== false && !empty(get_field('user_profile_picture', 'user_' . get_the_author_meta('ID'))))
-                    <?php
-                        $image_id = get_user_meta(get_the_author_meta('ID'), 'user_profile_picture_id', true);
-                        $image_size = array(64, 64);
-                        $image_url = wp_get_attachment_image_src($image_id, $image_size);
-                        $author_image = isset($image_url[0]) ? $image_url[0] : false;
-                    ?>
-                    <span class="post-author-image" style="background-image:url('{{ $author_image }}');"><img src="{{ $author_image }}" alt="{{ (!empty(get_the_author_meta('first_name')) && !empty(get_the_author_meta('last_name'))) ? get_the_author_meta('first_name') . ' ' . get_the_author_meta('last_name')  : get_the_author() }}"></span>
-                @endif
-
-                @if (!empty(get_the_author_meta('first_name')) && !empty(get_the_author_meta('last_name')))
-                    <span class="post-author-name">{!! apply_filters('Municipio/author_display/name', get_the_author_meta('first_name') . ' ' . get_the_author_meta('last_name'), get_the_author_meta('ID')) !!}</span>
-                @else
-                    <span class="post-author-name">{!! apply_filters('Municipio/author_display/name', get_the_author(), get_the_author_meta('ID')) !!}</span>
-                @endif
-            </span>
-        </li>
-    @endif
-
     @if (get_the_modified_time() != get_the_time())
 
         @if (is_array(get_field('show_date_updated','option')) && is_array(get_field('show_date_published','option')) && in_array(get_post_type(get_the_id()), get_field('show_date_updated','option')) && in_array(get_post_type(get_the_id()), get_field('show_date_published','option')))
@@ -55,5 +32,28 @@
             </li>
         @endif
 
+    @endif
+
+    @if (get_field('page_show_author', 'option') !== false && get_field('post_show_author', get_the_id()) !== false && get_the_author())
+        <li class="c-article-timestamps__list-item">
+            <strong><?php echo apply_filters('Municipio/author_display/title', __('Published by', 'municipio')); ?>:</strong>
+            <span class="post-author post-author-margin-left">
+                @if (get_field('page_show_author_image', 'option') !== false && get_field('post_show_author_image', get_the_id()) !== false && !empty(get_field('user_profile_picture', 'user_' . get_the_author_meta('ID'))))
+                    <?php
+                        $image_id = get_user_meta(get_the_author_meta('ID'), 'user_profile_picture_id', true);
+                        $image_size = array(64, 64);
+                        $image_url = wp_get_attachment_image_src($image_id, $image_size);
+                        $author_image = isset($image_url[0]) ? $image_url[0] : false;
+                    ?>
+                    <span class="post-author-image" style="background-image:url('{{ $author_image }}');"><img src="{{ $author_image }}" alt="{{ (!empty(get_the_author_meta('first_name')) && !empty(get_the_author_meta('last_name'))) ? get_the_author_meta('first_name') . ' ' . get_the_author_meta('last_name')  : get_the_author() }}"></span>
+                @endif
+
+                @if (!empty(get_the_author_meta('first_name')) && !empty(get_the_author_meta('last_name')))
+                    <span class="post-author-name">{!! apply_filters('Municipio/author_display/name', get_the_author_meta('first_name') . ' ' . get_the_author_meta('last_name'), get_the_author_meta('ID')) !!}</span>
+                @else
+                    <span class="post-author-name">{!! apply_filters('Municipio/author_display/name', get_the_author(), get_the_author_meta('ID')) !!}</span>
+                @endif
+            </span>
+        </li>
     @endif
 </ul>
