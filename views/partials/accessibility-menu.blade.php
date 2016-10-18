2f364747f159bb7e284caa73dea3e3a28b84eaf7
Peter Antonius <peter.antonius@whitespace.se>
2016-10-18 09:02:40 +0200

    Readspeaker focus fix

--- views/partials/accessibility-menu.blade.php
+++ views/partials/accessibility-menu.blade.php
@@ -1,9 +1,9 @@
 @if (function_exists('ReadSpeakerHelper_playButton'))
 <div class="c-accessibility-menu hidden-print" aria-label="<?php _e('Listen to this page with ReadSpeaker', 'hoor'); ?>">
     {!! ReadSpeakerHelper_playButton() !!}
-</div>
-@endif
 
-@if (function_exists('ReadSpeakerHelper_player'))
-    {!! ReadSpeakerHelper_player() !!}
+    @if (function_exists('ReadSpeakerHelper_player'))
+    	{!! ReadSpeakerHelper_player() !!}
+	@endif
+</div>
 @endif
