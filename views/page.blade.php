be21c7c3684515253eb670a82552576e387e5760
Peter Antonius <peter.antonius@whitespace.se>
2016-10-18 09:04:07 +0200

    Better Readspeaker placement

--- views/page.blade.php
+++ views/page.blade.php
@@ -13,7 +13,7 @@
     <div class="grid {{ (wp_get_post_parent_id(get_the_id()) != 0) ? 'no-margin-top' : '' }}">
         @include('partials.sidebar-left')
 
-        <div class="{{ $contentGridSize }} grid-print-12" id="readspeaker-read">
+        <div class="{{ $contentGridSize }} grid-print-12">
 
             @if (is_active_sidebar('content-area-top'))
                 <div class="sidebar-content-area sidebar-content-area-top">
@@ -23,11 +23,13 @@
                 </div>
             @endif
 
-            @while(have_posts())
-                {!! the_post() !!}
+            <div id="readspeaker-read">
+                @while(have_posts())
+                    {!! the_post() !!}
 
-                @include('partials.article')
-            @endwhile
+                    @include('partials.article')
+                @endwhile
+            </div>
 
             @if (is_active_sidebar('content-area'))
                 <div class="sidebar-content-area sidebar-content-area-bottom">
