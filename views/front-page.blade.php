50fd89a0da3990a19d0028321379435cc5dc902d
Peter Antonius <peter.antonius@whitespace.se>
2016-10-19 14:37:58 +0200

    Popular searches

--- views/front-page.blade.php
+++ views/front-page.blade.php
@@ -2,14 +2,59 @@
 
 @section('content')
 
-@if (is_active_sidebar('content-area'))
-<section class="creamy creamy-border-bottom gutter-xl gutter-vertical sidebar-content-area">
-    <div class="container">
-        <div class="grid">
-            <?php dynamic_sidebar('content-area'); ?>
-        </div>
-    </div>
-</section>
+@if (is_front_page() && is_array(get_field('search_display', 'option')) && in_array('hero', get_field('search_display', 'option')))
+    {{ get_search_form() }}
 @endif
+
+<div class="search-popular">
+	<div class="container">
+		<div class="grid">
+			<div class="grid-md-12">
+				<p class="search-popular__title"><?php _e('Popular searches', 'hoor'); ?>:</p>
+				<ul class="search-popular__list">
+					<li class="search-popular__list-item"><a class="search-popular__list-link" href="?s=Lediga jobb">Lediga jobb</a></li>
+					<li class="search-popular__list-item"><a class="search-popular__list-link" href="?s=Förskola">Förskola</a></li>
+					<li class="search-popular__list-item"><a class="search-popular__list-link" href="?s=Äldreomsorg">Äldreomsorg</a></li>
+					<li class="search-popular__list-item"><a class="search-popular__list-link" href="?s=Gymnasievalet">Gymnasievalet</a></li>
+					<li class="search-popular__list-item"><a class="search-popular__list-link" href="?s=Badplatser">Badplatser</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="container-front container-front--alt">
+    <code>[Hero] - Genvägar</code>
+    <div class="container">
+        @include('partials.hero')
+    </div>
+</div>
+
+<div class="container-front">
+    <code>[Content Area Top] - 4 Puffar</code>
+    <div class="container">
+        @if (is_active_sidebar('content-area-top'))
+            <?php dynamic_sidebar('content-area-top'); ?>
+        @endif
+    </div>
+</div>
+
+<div class="container-front container-front--alt">
+    <code>[Content Area] - Nyheter</code>
+    <div class="container">
+        @if (is_active_sidebar('content-area'))
+            <?php dynamic_sidebar('content-area'); ?>
+        @endif
+    </div>
+</div>
+
+<div class="container-front">
+    <code>[Right Sidebar] - Evenemang</code>
+    <div class="container">
+        @if (is_active_sidebar('right-sidebar'))
+            <?php dynamic_sidebar('right-sidebar'); ?>
+        @endif
+    </div>
+</div>
 
 @stop
