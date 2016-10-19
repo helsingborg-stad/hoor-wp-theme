6030aea8f1f59c888d8f51d95b03e86d897781b8
Peter Antonius <peter.antonius@whitespace.se>
2016-10-19 17:30:45 +0200

    WIP for front-page structure and style

--- views/front-page.blade.php
+++ views/front-page.blade.php
@@ -2,26 +2,50 @@
 
 @section('content')
 
-@if (is_front_page() && is_array(get_field('search_display', 'option')) && in_array('hero', get_field('search_display', 'option')))
-    {{ get_search_form() }}
-@endif
+<?php 
+    $image = get_field('hero_image');
 
-<div class="search-popular">
-	<div class="container">
-		<div class="grid">
-			<div class="grid-md-12">
-				<p class="search-popular__title"><?php _e('Popular searches', 'hoor'); ?>:</p>
-				<ul class="search-popular__list">
-					<li class="search-popular__list-item"><a class="search-popular__list-link" href="?s=Lediga jobb">Lediga jobb</a></li>
-					<li class="search-popular__list-item"><a class="search-popular__list-link" href="?s=Förskola">Förskola</a></li>
-					<li class="search-popular__list-item"><a class="search-popular__list-link" href="?s=Äldreomsorg">Äldreomsorg</a></li>
-					<li class="search-popular__list-item"><a class="search-popular__list-link" href="?s=Gymnasievalet">Gymnasievalet</a></li>
-					<li class="search-popular__list-item"><a class="search-popular__list-link" href="?s=Badplatser">Badplatser</a></li>
-				</ul>
-			</div>
-		</div>
-	</div>
-</div>
+    if( !empty($image) ): ?>
+
+    <div class="hero-image">
+        <style>
+            .hero-image {
+                background-image: url(<?php echo $image['url']; ?>);
+            }
+        </style>
+
+        <div class="hero-search">
+            <form class="c-search-form" method="get" action="/">
+                <label for="searchkeyword-{{ $searchFormNode }}" class="visually-hidden">{{ get_field('search_label_text', 'option') ? get_field('search_label_text', 'option') : __('Search', 'municipio') }}</label>
+                <div class="c-search-form__group">
+                    <input id="searchkeyword-{{ $searchFormNode }}" autocomplete="off" class="c-search-form__field" type="search" name="s" placeholder="{{ get_field('search_placeholder_text', 'option') ? get_field('search_placeholder_text', 'option') : 'What are you looking for?' }}" value="<?php echo (!empty(get_search_query())) ? get_search_query() : ''; ?>">
+                    <button type="submit" class="c-search-form__button c-search-form__button--primary">{{ get_field('search_button_text', 'option') ? get_field('search_button_text', 'option') : __('Search', 'municipio') }}</button>
+                </div>
+            </form>
+        </div>
+    </div>
+
+
+<?php endif; ?>
+
+<?php if( have_rows('search_links') ): ?>
+    <div class="search-popular">
+        <div class="container">
+            <div class="grid">
+                <div class="grid-md-12">
+                    <p class="search-popular__title"><?php _e('Popular searches', 'hoor'); ?>:</p>
+                    <ul class="search-popular__list">
+                        <?php while ( have_rows('search_links') ) : the_row(); ?>
+                            <li class="search-popular__list-item"><a class="search-popular__list-link" href="?s={{ rawurlencode(get_sub_field('search_query')) }}">{{ get_sub_field('search_label') }}</a></li>
+                        <?php endwhile; ?>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php else :
+    // no rows found
+endif; ?>
 
 <div class="container-front container-front--alt">
     <code>[Hero] - Genvägar</code>
