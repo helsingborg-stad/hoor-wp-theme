85aad1a0df9248c089a2771146f00a4ad4e9ae8e
Anders Fajerson <anders@perifer.se>
2016-10-20 08:15:10 +0200

    Use default footer template instead of hoor footer
    The hoor footer was not used on search, 404, archive etc - pages
    that have controller extending \Municipio\Controller\BaseController
    thus not triggering our BaseController.

--- views/partials/footer/footer-default.blade.php
+++ views/partials/footer/footer-default.blade.php
@@ -1,45 +1,9 @@
 <footer class="main-footer">
     <div class="container">
-
-        <div class="grid">
-            <div class="{{ get_field('footer_signature_show', 'option') ? 'grid-md-10' : 'grid-md-12' }}">
-
-                {{-- ## Footer widget area begin ## --}}
-                <div class="grid sidebar-footer-area">
-                    @if (is_active_sidebar('footer-area'))
-                        <?php dynamic_sidebar('footer-area'); ?>
-                    @endif
-                </div>
-                {{-- ## Footer widget area end ## --}}
-
-                {{-- ## Footer header begin ## --}}
-                @if (get_field('footer_logotype_vertical_position', 'option') == 'bottom' && get_field('footer_logotype', 'option') != 'hide')
-                <div class="grid no-margin-top">
-                    <div class="grid-md-6">
-                        {!! municipio_get_logotype(get_field('footer_logotype', 'option')) !!}
-                    </div>
-                    <div class="grid-md-6">
-                        @if(have_rows('footer_icons_repeater', 'option'))
-                            <ul class="icons-list">
-                                @foreach(get_field('footer_icons_repeater', 'option') as $link)
-                                    <li>
-                                        <a href="{{ $link['link_url'] }}" target="_blank" class="link-item-light">
-                                            {!! $link['link_icon'] !!}
-
-                                            @if (isset($link['link_title']))
-                                            <span class="sr-only">{{ $link['link_title'] }}</span>
-                                            @endif
-                                        </a>
-                                    </li>
-                                @endforeach
-                            </ul>
-                        @endif
-                    </div>
-                </div>
-                @endif
-                {{-- ## Footer header end ## --}}
-
-            </div>
+        <div class="grid sidebar-footer-area">
+            @if (is_active_sidebar('footer-area'))
+                <?php dynamic_sidebar('footer-area'); ?>
+            @endif
         </div>
     </div>
 </footer>
