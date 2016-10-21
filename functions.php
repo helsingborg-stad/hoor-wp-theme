331ae216650c98e2de162f867010cb5e2aea994f
Anders Fajerson <anders@perifer.se>
2016-10-21 21:50:19 +0200

    Collect and add nginx redirects

--- functions.php
+++ functions.php
@@ -123,3 +123,51 @@ function hoor_theme_setup() {
 }
 
 load_child_theme_textdomain( 'hoor', get_stylesheet_directory() . '/languages' );
+
+
+/**
+ * Quick hack to render output collected redirects.
+ * TODO: Remove this after the site has been released.
+ */
+function output_nginx_redirects() {
+    if (is_super_admin() && isset($_GET['nginx-conf'])) {
+        /*
+
+        location = /content/unique-page-name {
+          return 301 /new-name/unique-page-name;
+        }
+
+        */
+
+        $output = '';
+        $error = '';
+        $posts = get_posts(array(
+            'numberposts'   => 10,
+            'post_type'     => 'page',
+            'meta_key'      => 'redirect_on',
+            'meta_value'    => 'Ja'
+        ));
+
+        foreach ($posts as $post) {
+            $old_url = get_field('redirect_url', $post->ID);
+            $old_url_relative = str_replace('http://www.hoor.se', '', $old_url);
+            $permalink = get_permalink($post->ID);
+            $permalink_relative = str_replace(home_url(), '', $permalink);
+            $permalink_normalized = rtrim(str_replace(home_url(), '', $permalink), '/');
+            if ($old_url == $old_url_relative || $old_url_relative == $permalink_relative || $old_url_relative == $permalink_normalized) {
+                $error .= "$old_url. <a href=\"$permalink\"><small>View page</small></a></br>";
+            }
+            else {
+                $output .= "location = $old_url_relative {\n  return 301 $permalink_relative;\n}\n";
+            }
+        }
+
+        echo "<p style=\"color:red\">The following redirect(s) are incorrect:<br/>$error</pre></p>";
+        echo '<strong>Nginx conf:</strong>';
+        echo "<pre>$output<pre>";
+        die;
+    }
+}
+output_nginx_redirects();
+
+
