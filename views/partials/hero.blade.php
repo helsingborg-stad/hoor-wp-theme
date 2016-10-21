5fdc132d94335eba8c223df64c3d640c3ba7ea9f
Peter Antonius <peter.antonius@whitespace.se>
2016-10-21 14:14:28 +0200

    Remove search box from front-page

--- views/partials/hero.blade.php
+++ views/partials/hero.blade.php
@@ -1,11 +1,9 @@
 @if (is_active_sidebar('slider-area') === true )
-    <div class="hero container">
-		<div class="grid">
-		    <?php dynamic_sidebar('slider-area'); ?>
+    <div class="hero">
+        <div class="container">
+            <div class="grid">
+                <?php dynamic_sidebar('slider-area'); ?>
+            </div>
         </div>
-
-        @if (is_front_page() && is_array(get_field('search_display', 'option')) && in_array('hero', get_field('search_display', 'option')))
-            {{ get_search_form() }}
-        @endif
     </div>
 @endif
