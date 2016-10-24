3b21dcae7dda0fe7f23946b407071c56fd275891
Peter Antonius <peter.antonius@whitespace.se>
2016-10-24 19:15:24 +0200

    Updated to latest version of modularity template. Added hoor classes

--- templates/module/modularity-mod-image.php
+++ templates/module/modularity-mod-image.php
@@ -6,23 +6,22 @@ if ($fields['mod_image_crop'] === true) {
     $imageSrc = wp_get_attachment_image_src(
         $fields['mod_image_image']['ID'],
         apply_filters('Modularity/image/image',
-            array($fields['mod_image_crop_width'], $fields['mod_image_crop_height'])
+            municipio_to_aspect_ratio('16:9', array($fields['mod_image_crop_width'], $fields['mod_image_crop_height']))
         )
     );
-
     $imageSrc = $imageSrc[0];
 } else {
     $imageSrc = $fields['mod_image_image']['sizes'][$fields['mod_image_size']];
 }
-
 $classes = array();
-
 if ($fields['mod_image_responsive'] === true) {
     $classes[] = 'image-responsive';
 }
-
+if (!$module->hideTitle) {
+    echo '<h2 class="box__headline">' . $module->post_title . '</h2>';
+}
 if (isset($fields['mod_image_link_url']) && strlen($fields['mod_image_link_url']) > 0) {
-    echo '<div class="box box__image"><a href="' . $fields['mod_image_link_url'] . '"><img src="' . $imageSrc . '" alt="' . $fields['mod_image_image']['alt'] . '" class="' . implode(' ', apply_filters('', $classes)) . '"></a></div>';
+    echo '<a href="' . $fields['mod_image_link_url'] . '"><img class="box box__image" src="' . $imageSrc . '" alt="' . $fields['mod_image_image']['alt'] . '" class="' . implode(' ', apply_filters('', $classes)) . '"></a>';
 } else {
-    echo '<div class="box box__image"><img src="' . $imageSrc . '" alt="' . $fields['mod_image_image']['alt'] . '" class="' . implode(' ', apply_filters('', $classes)) . '"></div>';
+    echo '<img class="box box__image" src="' . $imageSrc . '" alt="' . $fields['mod_image_image']['alt'] . '" class="' . implode(' ', apply_filters('', $classes)) . '">';
 }
