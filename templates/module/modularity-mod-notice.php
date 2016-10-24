20978cbde93d668d2de8dc6bd5a5631c006b6a5e
Peter Antonius <peter.antonius@whitespace.se>
2016-10-24 16:49:22 +0200

    Better grid handling of notice box

--- templates/module/modularity-mod-notice.php
+++ templates/module/modularity-mod-notice.php
@@ -1,11 +1,11 @@
 <?php $fields = get_fields($module->ID); ?>
 <div class="grid">
-	<div class="grid-sm-12">
-		<div class="notice <?php echo $fields['notice_type']; ?> <?php echo $fields['notice_size']; ?>">
-		    <?php if (!$module->hideTitle) : ?>
-		        <h2 class="notice__title"><?php echo $module->post_title; ?></h2>
-		    <?php endif; ?>
-		    <p class="notice__description"><?php echo $fields['notice_text']; ?></p>
-		</div>
-	</div>
+    <div class="grid-sm-12">
+        <div class="notice <?php echo $fields['notice_type']; ?> <?php echo $fields['notice_size']; ?>">
+            <?php if (!$module->hideTitle) : ?>
+                <h2 class="notice__title"><?php echo $module->post_title; ?></h2>
+            <?php endif; ?>
+            <p class="notice__description"><?php echo $fields['notice_text']; ?></p>
+        </div>
+    </div>
 </div>
