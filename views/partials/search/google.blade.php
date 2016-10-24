913e10de0ec9704e4b080e223a69ce5a5cfc92b6
Peter Antonius <peter.antonius@whitespace.se>
2016-10-24 22:56:42 +0200

    Hide URLs in SERP

--- views/partials/search/google.blade.php
+++ views/partials/search/google.blade.php
@@ -48,9 +48,11 @@
                                 <li class="search-result__item">
                                     <h3 class="search-result__title"><a class="search-result__link {{ (isset($item->fileFormat)) ? $search->getFiletypeClass($item->fileFormat) : '' }}" href="{{ apply_filters('Municipio/search_result/permalink_url', $item->link, false) }}">{!! apply_filters('Municipio/search_result/title', $item->htmlTitle, false) !!}</a></h3>
                                     <p class="search-result__excerpt">@if ($search->getModifiedDate($item))<span class="search-result__date"><time >{{ apply_filters('Municipio/search_result/date', $search->getModifiedDate($item), false) }}</time> &dash; </span>@endif{!! apply_filters('Municipio/search_result/excerpt', trim($item->snippet), false) !!}</p>
+                                    <!--
                                     <div class="search-result__details">
                                         <span ><a href="{{ apply_filters('Municipio/search_result/permalink_url', $item->link, false) }}" class="search-result__url">{!! apply_filters('Municipio/search_result/permalink_text', $item->htmlFormattedUrl, false) !!}</a>
                                     </div>
+                                    -->
                                 </li>
                                 @endforeach
 
