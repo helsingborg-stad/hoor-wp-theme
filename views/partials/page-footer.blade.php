87c7c00613b2669a5e77c3d14192220bec9dacfb
Anders Fajerson <anders@perifer.se>
2016-10-25 13:02:11 +0200

    Fix post_show_share setting on pages

--- views/partials/page-footer.blade.php
+++ views/partials/page-footer.blade.php
@@ -1,7 +1,7 @@
 <?php do_action('customer-feedback'); ?>
 
 <footer class="c-page-footer">
-    @if (get_field('show_share', get_the_id()) !== false && get_field('page_show_share', 'option') !== false)
+    @if (get_field('post_show_share', get_the_id()) !== false && get_field('page_show_share', 'option') !== false)
     <div class="c-share hidden-print clearfix">
 
         <p class="c-share__title visually-hidden" id="social-share"><strong><?php _e('Share the page', 'municipio'); ?>:</strong> {{ the_title() }}</p>
