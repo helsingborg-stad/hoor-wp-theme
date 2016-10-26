6d08177cc37c3a9908fa0c85ae7534b02c7b045a
Peter Antonius <peter.antonius@whitespace.se>
2016-10-26 14:28:17 +0200

    Fix for duplicate grid classes

--- templates/module/modularity-mod-notice.php
+++ templates/module/modularity-mod-notice.php
@@ -1,11 +1,9 @@
 <?php $fields = get_fields($module->ID); ?>
-<div class="notice <?php echo $fields['notice_type']; ?> <?php echo $fields['notice_size']; ?>">
-    <div class="grid">
-            <div class="grid-sm-12">
-            <?php if (!$module->hideTitle) : ?>
-                <h2 class="notice__title"><?php echo $module->post_title; ?></h2>
-            <?php endif; ?>
-            <p class="notice__description"><?php echo $fields['notice_text']; ?></p>
-        </div>
+<div class="grid-sm-12">
+    <div class="notice <?php echo $fields['notice_type']; ?> <?php echo $fields['notice_size']; ?>">
+        <?php if (!$module->hideTitle) : ?>
+            <h2 class="notice__title"><?php echo $module->post_title; ?></h2>
+        <?php endif; ?>
+        <p class="notice__description"><?php echo $fields['notice_text']; ?></p>
     </div>
 </div>
