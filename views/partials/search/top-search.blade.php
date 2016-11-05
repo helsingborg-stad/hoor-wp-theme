101b89eff37a79ea21c5b0dfe2e2369da285a8c4
Peter Antonius <peter.antonius@whitespace.se>
2016-11-05 21:29:18 +0100

    Fix a11y problem with search label

--- views/partials/search/top-search.blade.php
+++ views/partials/search/top-search.blade.php
@@ -3,7 +3,7 @@
     $searchFormNode = ($searchFormNode) ? $searchFormNode+1 : 1;
 ?>
 <form class="c-search-form" method="get" action="/">
-    <label for="searchkeyword-{{ $searchFormNode }}" class="visually-hidden">{{ get_field('search_label_text', 'option') ? get_field('search_label_text', 'option') : __('Search', 'municipio') }}</label>
+    <label for="searchkeyword-{{ $searchFormNode }}" class="visually-hidden c-search-form__label">{{ get_field('search_label_text', 'option') ? get_field('search_label_text', 'option') : __('Search', 'municipio') }}</label>
     <div class="c-search-form__group">
         <input id="searchkeyword-{{ $searchFormNode }}" autocomplete="off" class="c-search-form__field" type="search" name="s" placeholder="{{ get_field('search_placeholder_text', 'option') ? get_field('search_placeholder_text', 'option') : 'What are you looking for?' }}" value="<?php echo (!empty(get_search_query())) ? get_search_query() : ''; ?>">
         <button type="submit" class="c-search-form__button">{{ get_field('search_button_text', 'option') ? get_field('search_button_text', 'option') : __('Search', 'municipio') }}</button>
