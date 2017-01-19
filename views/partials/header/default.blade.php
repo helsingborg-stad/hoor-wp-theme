ffb86b7baff7256d059ae389a87199e3e1146632
Peter Antonius <peter.antonius@whitespace.se>
2017-01-19 14:56:57 +0100

    Dummy example for responsive header navigation

--- views/partials/header/default.blade.php
+++ views/partials/header/default.blade.php
@@ -1,45 +1,55 @@
-<div class="container">
-    <div class="grid">
-        <div class="grid-xs-12 grid-sm-3 site-header__logotype">
-            {!! municipio_get_logotype(get_field('header_logotype', 'option'), get_field('logotype_tooltip', 'option'), false, get_field('header_tagline_enable', 'option')) !!}
-        </div>
-        <div class="grid-xs-12 grid-sm-9 hidden-print">
-            {!!
-                wp_nav_menu(array(
-                    'theme_location' => 'help-menu',
-                    'container' => 'nav',
-                    'container_class' => 'menu-help',
-                    'container_id' => '',
-                    'menu_class' => 'nav nav-help nav-horizontal',
-                    'menu_id' => 'help-menu-top',
-                    'echo' => 'echo',
-                    'before' => '',
-                    'after' => '',
-                    'link_before' => '',
-                    'link_after' => '',
-                    'items_wrap' => '<ul class="%2$s">%3$s</ul>',
-                    'depth' => 1,
-                    'fallback_cb' => '__return_false'
-                ));
-            !!}
-        </div>
+<div class="mobile-header">
+    <div class="mobile-header__logotype">
+        {!! municipio_get_logotype(get_field('header_logotype', 'option'), get_field('logotype_tooltip', 'option'), false, get_field('header_tagline_enable', 'option')) !!}
     </div>
+    <button aria-expanded="false" aria-controls="site-header-container" class=""><?php _e('Menu', 'municipio'); ?></button>
+    <button aria-expanded="false" aria-controls="search" class=""><?php _e('Search', 'municipio'); ?></button>
 </div>
 
-@if (get_field('nav_primary_enable', 'option') === true)
-    <nav class="primary-navigation hidden-print">
-        <div class="primary-navigation__container clearfix">
-            <?php
-                // {!! $navigation['mainMenu'] !!}
-                // Ideally the above should be used if Municipio
-                // is updated so we don't need our helpers that
-                // override Municipio´s helpers.
-                $navigation = new \Hoor\Helper\Navigation();
-                echo $navigation->mainMenu();
-             ?>
+<div id="site-header-container" class="site-header-container">
+    <div class="container">
+        <div class="grid">
+            <div class="grid-xs-12 grid-sm-3 site-header__logotype">
+                {!! municipio_get_logotype(get_field('header_logotype', 'option'), get_field('logotype_tooltip', 'option'), false, get_field('header_tagline_enable', 'option')) !!}
+            </div>
+            <div class="grid-xs-12 grid-sm-9 hidden-print">
+                {!!
+                    wp_nav_menu(array(
+                        'theme_location' => 'help-menu',
+                        'container' => 'nav',
+                        'container_class' => 'menu-help',
+                        'container_id' => '',
+                        'menu_class' => 'nav nav-help nav-horizontal',
+                        'menu_id' => 'help-menu-top',
+                        'echo' => 'echo',
+                        'before' => '',
+                        'after' => '',
+                        'link_before' => '',
+                        'link_after' => '',
+                        'items_wrap' => '<ul class="%2$s">%3$s</ul>',
+                        'depth' => 1,
+                        'fallback_cb' => '__return_false'
+                    ));
+                !!}
+            </div>
         </div>
-    </nav>
-@endif
+    </div>
+
+    @if (get_field('nav_primary_enable', 'option') === true)
+        <nav class="primary-navigation hidden-print">
+            <div class="primary-navigation__container clearfix">
+                <?php
+                    // {!! $navigation['mainMenu'] !!}
+                    // Ideally the above should be used if Municipio
+                    // is updated so we don't need our helpers that
+                    // override Municipio´s helpers.
+                    $navigation = new \Hoor\Helper\Navigation();
+                    echo $navigation->mainMenu();
+                 ?>
+            </div>
+        </nav>
+    @endif
+</div>
 
 <div class="search-top header__search hidden-print" id="search">
     <div class="container">
