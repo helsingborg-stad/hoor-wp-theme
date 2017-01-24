dd012fa36763ac36170038473e6fc7500126b039
Peter Antonius <peter.antonius@whitespace.se>
2017-01-25 00:37:57 +0100

    Event structure and style

--- views/archive-evenemang.blade.php
+++ views/archive-evenemang.blade.php
@@ -1,6 +1,5 @@
 {{-- This template is used for the event archive page --}}
 
-
 @extends('templates.master')
 
 @section('content')
@@ -14,6 +13,7 @@
 <div class="container main-container">
 
     <div class="grid">
+
         <div class="grid-lg-12">
             <div class="grid">
 
@@ -27,33 +27,67 @@
                         </div>
                     @endif
 
-                    @while(have_posts())
-                        <h1>Evenemang</h1>
-                        {!! the_post() !!}
+                    <h1><?php _e('Events', 'hoor'); ?></h1>
 
-                        <?php global $post; ?>
-                        <div class="grid">
-                            <div class="grid-xs-12">
-                                <div class="post post-single">
-                                    <h2><a href="{{ the_permalink() }}">{{ the_title() }}</a></h2>
-                                    <h3>{{ get_field(date) }}</h3>
-                                    <h3>{{ get_field(place) }}</h3>
+                    <ul class="archive archive__event">
+                        @while(have_posts())
+                            {!! the_post() !!}
 
-                                    <article class="clearfix">
-                                        @if (isset(get_extended($post->post_content)['main']) && strlen(get_extended($post->post_content)['main']) > 0 && isset(get_extended($post->post_content)['extended']) && strlen(get_extended($post->post_content)['extended']) > 0)
+                            <?php global $post; ?>
+                            <li class="archive__item">
+                                <div class="archive__container">
+                                    <div>
+                                        <h3 class="archive__headline"><a href="{{ the_permalink() }}">{{ the_title() }}</a></h3>
+                                        <div class="archive__meta">
+                                            <p class="archive__meta--date">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 512 512"><path d="M144 128c17.7 0 32-14.3 32-32V64c0-17.7-14.3-32-32-32s-32 14.3-32 32v32c0 17.7 14.3 32 32 32zm224 0c17.7 0 32-14.3 32-32V64c0-17.7-14.3-32-32-32s-32 14.3-32 32v32c0 17.7 14.3 32 32 32z"/><path d="M472 64h-56v40.7c0 22.5-23.2 39.3-47.2 39.3S320 127.2 320 104.7V64H192v40.7c0 22.5-24 39.3-48 39.3s-48-16.8-48-39.3V64H40c-4.4 0-8 3.6-8 8v400c0 4.4 3.6 8 8 8h432c4.4 0 8-3.6 8-8V72c0-4.4-3.6-8-8-8zm-40 368H80V176h352v256z"/></svg>
+                                                <strong><?php _e('Date', 'hoor'); ?>:</strong>
+                                                <?php echo date('d', $startdate); ?>
+                                                <?php $startdate = strtotime(get_field('event_startdate', false, false)) ?>
+                                                    <time class="archive__meta--time" datetime="<?php echo date('Y-m-d\TH:i', $startdate); ?>">
+                                                        <?php echo date_i18n('F', $startdate); ?>
+                                                        <?php echo date('H:i', $startdate); ?>
+                                                    </time>
+                                                <?php if ($endate = get_field('event_enddate', false, false)): ?>
+                                                    -
+                                                    <?php $enddate = strtotime($endate) ?>
+                                                    <time  datetime="<?php echo date('Y-m-d\TH:i', $enddate); ?>">
+                                                      <?php echo date('d', $enddate); ?>
+                                                      <?php echo date_i18n('F', $enddate); ?>
+                                                      <?php echo date('H:i', $enddate); ?>
+                                                    </time>
+                                                <?php endif ?>
+                                            </p>
 
-                                            {!! apply_filters('the_lead', get_extended($post->post_content)['main']) !!}
-                                            {!! apply_filters('the_content', get_extended($post->post_content)['extended']) !!}
+                                            <?php if ($place = get_field('event_place')): ?>
+                                                <p class="archive__meta--place">
+                                                    <svg width="14" height="16" viewBox="0 0 14 20" xmlns="http://www.w3.org/2000/svg"><path d="M7 0C3.13 0 0 3.13 0 7c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5a2.5 2.5 0 1 1 0-5 2.5 2.5 0 0 1 0 5z"fill-rule="evenodd"/></svg>
+                                                    <strong><?php _e('Location', 'hoor'); ?>:</strong> <?php echo $place ?>
+                                                </p>
+                                            <?php endif ?>
+                                        </div>
 
-                                        @else
-                                            {!! the_content() !!}
-                                        @endif
-                                    </article>
+                                        {{ the_excerpt() }}
+                                    </div>
+                                    @if (municipio_get_thumbnail_source(null,array(400,250)))
+                                        <div class="archive__image-container">
+                                            <img src="{{ municipio_get_thumbnail_source(null,array(200,250)) }}">
+                                        </div>
+                                    @endif
                                 </div>
-                            </div>
-                        </div>
-                    @endwhile
+                            </li>
+                        @endwhile
+                    </ul>
 
+                    <div class="grid">
+                        <div class="grid-lg-12">
+                            {!!
+                                paginate_links(array(
+                                    'type' => 'list'
+                                ))
+                            !!}
+                        </div>
+                    </div>
 
                     @if (is_active_sidebar('content-area'))
                         <div class="sidebar-content-area sidebar-content-area-bottom">
--- views/single-evenemang.blade.php
+++ views/single-evenemang.blade.php
@@ -9,75 +9,95 @@
 <div class="container main-container">
 
     <div class="grid {{ (wp_get_post_parent_id(get_the_id()) != 0) ? 'no-margin-top' : '' }}">
-        @include('partials.sidebar-left')
+        <div class="grid-lg-3 hidden-print">
+            @include('partials.sidebar-left')
+        </div>
 
-        <div class="{{ $contentGridSize }} grid-print-12">
+        <div class="grid-lg-9">
+            <div class="grid">
 
-            @if (is_active_sidebar('content-area-top'))
-                <div class="sidebar-content-area sidebar-content-area-top">
-                    <div class="grid">
-                        <?php dynamic_sidebar('content-area-top'); ?>
-                    </div>
-                </div>
-            @endif
-
-            <div id="readspeaker-read">
-                @while(have_posts())
-                    {!! the_post() !!}
-
-                    <?php global $post; ?>
-                    <article class="clearfix">
-                        <h1>{{ the_title() }}</h1>
-
-                        @include('partials.accessibility-menu')
-
-                        <div class="box__date-event">
-                            <?php $startdate = strtotime(get_field('event_startdate', false, false)) ?>
-                                <time class="box__date-event" datetime="<?php echo date('Y-m-d\TH:i', $startdate); ?>">
-                                <span class="box__date-event-start--day"><?php echo date('d', $startdate); ?></span>
-                                <span class="box__date-event-start--month"><?php echo date_i18n('F', $startdate); ?></span>
-                                <span class="box__date-event-start--time"><?php echo date('H:i', $startdate); ?></span>
-                            </time>
-                            <?php if ($endate = get_field('event_enddate', false, false)): ?>
-                                -
-                                <?php $enddate = strtotime($endate) ?>
-                                <time class="box__date-event-end" datetime="<?php echo date('Y-m-d\TH:i', $enddate); ?>">
-                                  <span class="box__date-event-start--day"><?php echo date('d', $enddate); ?></span>
-                                  <span class="box__date-event-start--month"><?php echo date_i18n('F', $enddate); ?></span>
-                                  <span class="box__date-event-end--time"><?php echo date('H:i', $enddate); ?></span>
-                                </time>
-                            <?php endif ?>
+                <div class="grid-sm-12 grid-md-8 grid-print-12">
+                    @if (is_active_sidebar('content-area-top'))
+                        <div class="sidebar-content-area sidebar-content-area-top">
+                            <div class="grid">
+                                <?php dynamic_sidebar('content-area-top'); ?>
+                            </div>
                         </div>
+                    @endif
 
-                        <?php if ($place = get_field('event_place')): ?>
-                            <p><?php echo $place ?><p>
-                        <?php endif ?>
+                    <div id="readspeaker-read">
+                        @while(have_posts())
+                            {!! the_post() !!}
 
-                        @if (isset(get_extended($post->post_content)['main']) && strlen(get_extended($post->post_content)['main']) > 0 && isset(get_extended($post->post_content)['extended']) && strlen(get_extended($post->post_content)['extended']) > 0)
+                            <?php global $post; ?>
+                            <article class="clearfix">
 
-                            {!! apply_filters('the_lead', get_extended($post->post_content)['main']) !!}
-                            {!! apply_filters('the_content', get_extended($post->post_content)['extended']) !!}
+                                @if (municipio_get_thumbnail_source(null,array(590,500)))
+                                    <div class="event__image-container">
+                                        <img src="{{ municipio_get_thumbnail_source(null,array(590,500)) }}">
+                                    </div>
+                                @endif
 
-                        @else
+                                <h1>{{ the_title() }}</h1>
 
-                            {!! the_content() !!}
+                                @include('partials.accessibility-menu')
 
-                        @endif
+                                <div class="event__meta">
+                                    <p class="event__meta--date">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 512 512"><path d="M144 128c17.7 0 32-14.3 32-32V64c0-17.7-14.3-32-32-32s-32 14.3-32 32v32c0 17.7 14.3 32 32 32zm224 0c17.7 0 32-14.3 32-32V64c0-17.7-14.3-32-32-32s-32 14.3-32 32v32c0 17.7 14.3 32 32 32z"/><path d="M472 64h-56v40.7c0 22.5-23.2 39.3-47.2 39.3S320 127.2 320 104.7V64H192v40.7c0 22.5-24 39.3-48 39.3s-48-16.8-48-39.3V64H40c-4.4 0-8 3.6-8 8v400c0 4.4 3.6 8 8 8h432c4.4 0 8-3.6 8-8V72c0-4.4-3.6-8-8-8zm-40 368H80V176h352v256z"/></svg>
+                                        <strong><?php _e('Date', 'hoor'); ?>:</strong>
+                                        <?php echo date('d', $startdate); ?>
+                                        <?php $startdate = strtotime(get_field('event_startdate', false, false)) ?>
+                                            <time class="event__meta--time" datetime="<?php echo date('Y-m-d\TH:i', $startdate); ?>">
+                                                <?php echo date_i18n('F', $startdate); ?>
+                                                <?php echo date('H:i', $startdate); ?>
+                                            </time>
+                                        <?php if ($endate = get_field('event_enddate', false, false)): ?>
+                                            -
+                                            <?php $enddate = strtotime($endate) ?>
+                                            <time  datetime="<?php echo date('Y-m-d\TH:i', $enddate); ?>">
+                                              <?php echo date('d', $enddate); ?>
+                                              <?php echo date_i18n('F', $enddate); ?>
+                                              <?php echo date('H:i', $enddate); ?>
+                                            </time>
+                                        <?php endif ?>
+                                    </p>
 
-                    </article>
+                                    <?php if ($place = get_field('event_place')): ?>
+                                        <p class="event__meta--place">
+                                            <svg width="14" height="16" viewBox="0 0 14 20" xmlns="http://www.w3.org/2000/svg"><path d="M7 0C3.13 0 0 3.13 0 7c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5a2.5 2.5 0 1 1 0-5 2.5 2.5 0 0 1 0 5z"fill-rule="evenodd"/></svg>
+                                            <strong><?php _e('Location', 'hoor'); ?>:</strong> <?php echo $place ?>
+                                        </p>
+                                    <?php endif ?>
+                                </div>
 
-                @endwhile
-            </div>
+                                @if (isset(get_extended($post->post_content)['main']) && strlen(get_extended($post->post_content)['main']) > 0 && isset(get_extended($post->post_content)['extended']) && strlen(get_extended($post->post_content)['extended']) > 0)
 
-            @if (is_active_sidebar('content-area'))
-                <div class="sidebar-content-area sidebar-content-area-bottom">
-                    <div class="grid">
-                        <?php dynamic_sidebar('content-area'); ?>
+                                    {!! apply_filters('the_lead', get_extended($post->post_content)['main']) !!}
+                                    {!! apply_filters('the_content', get_extended($post->post_content)['extended']) !!}
+
+                                @else
+
+                                    {!! the_content() !!}
+
+                                @endif
+
+                            </article>
+
+                        @endwhile
                     </div>
-                </div>
-            @endif
 
-            @include('partials.page-footer')
+                    @if (is_active_sidebar('content-area'))
+                        <div class="sidebar-content-area sidebar-content-area-bottom">
+                            <div class="grid">
+                                <?php dynamic_sidebar('content-area'); ?>
+                            </div>
+                        </div>
+                    @endif
+
+                    @include('partials.page-footer')
+                </div>
+            </div>
         </div>
 
 
