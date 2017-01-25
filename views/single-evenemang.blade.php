c65950f6901f8d681051f4044aabec362208e36f
Anders Fajerson <anders@perifer.se>
2017-01-25 15:54:01 +0100

    Fix startdate and remove padding 0 in day

--- views/single-evenemang.blade.php
+++ views/single-evenemang.blade.php
@@ -46,9 +46,9 @@
                                     <p class="event__meta--date">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 512 512"><path d="M144 128c17.7 0 32-14.3 32-32V64c0-17.7-14.3-32-32-32s-32 14.3-32 32v32c0 17.7 14.3 32 32 32zm224 0c17.7 0 32-14.3 32-32V64c0-17.7-14.3-32-32-32s-32 14.3-32 32v32c0 17.7 14.3 32 32 32z"/><path d="M472 64h-56v40.7c0 22.5-23.2 39.3-47.2 39.3S320 127.2 320 104.7V64H192v40.7c0 22.5-24 39.3-48 39.3s-48-16.8-48-39.3V64H40c-4.4 0-8 3.6-8 8v400c0 4.4 3.6 8 8 8h432c4.4 0 8-3.6 8-8V72c0-4.4-3.6-8-8-8zm-40 368H80V176h352v256z"/></svg>
                                         <strong><?php _e('Date', 'hoor'); ?>:</strong>
-                                        <?php echo date('d', $startdate); ?>
                                         <?php $startdate = strtotime(get_field('event_startdate', false, false)) ?>
                                             <time class="event__meta--time" datetime="<?php echo date('Y-m-d\TH:i', $startdate); ?>">
+                                                <?php echo date('j', $startdate); ?>
                                                 <?php echo date_i18n('F', $startdate); ?>
                                                 <?php echo date('H:i', $startdate); ?>
                                             </time>
@@ -56,7 +56,7 @@
                                             -
                                             <?php $enddate = strtotime($endate) ?>
                                             <time  datetime="<?php echo date('Y-m-d\TH:i', $enddate); ?>">
-                                              <?php echo date('d', $enddate); ?>
+                                              <?php echo date('j', $enddate); ?>
                                               <?php echo date_i18n('F', $enddate); ?>
                                               <?php echo date('H:i', $enddate); ?>
                                             </time>
