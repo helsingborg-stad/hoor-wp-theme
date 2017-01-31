91d3d438bb7f400710a50029bd0dfc44baebce67
Anders Fajerson <anders@perifer.se>
2017-01-31 13:37:12 +0100

    Fix path to cachebusted icons

--- views/partials/icon.blade.php
+++ views/partials/icon.blade.php
@@ -1,1 +1,1 @@
-<svg class="o-icon o-icon--{{ $name }}"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="<?php echo get_stylesheet_directory_uri() ?>/assets/dist/images/icons.svg#{{ $name }}"></use></svg>
+<svg class="o-icon o-icon--{{ $name }}"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="<?php echo get_stylesheet_directory_uri() ?>/assets/dist/{{ \Hoor\Helper\CacheBust::name('images/icons.svg') }}#{{ $name }}"></use></svg>
